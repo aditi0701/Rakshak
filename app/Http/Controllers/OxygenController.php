3f2617b481428e4c0360bf62662f11e7688f279a
Aishwaryabhootra <64602479+Aishwaryabhootra@users.noreply.github.com>
2021-10-20 11:26:13 +0530

    Update OxygenController.php

--- app/Http/Controllers/OxygenController.php
+++ app/Http/Controllers/OxygenController.php
@@ -22,6 +22,30 @@ class OxygenController extends Controller
         ->get();
         $array = json_decode($exist);
         
+        //validation
+        $req->validate([
+            'fname'=>['bail','required','max:15'],
+            'lname'=>['required','max:15'],
+            'orgname'=>['required','max:30'],
+            'contactno'=>['required','numeric','digits:10','regex:/^([0-9\s\-\+\(\)]*)$/'],
+            'city' =>['required','max:20'],
+            'state'=>['required','max:30'],
+            'address'=>['required','max:255'],
+            'quantity'=>['required','numeric','max:3']
+        ],
+        [
+            'fname'=>'Max of 15 characters allowed in FirstName',
+            'lname'=>'Max of 15 characters allowed in LastName',
+            'orgname'=>'Max of 30 characters allowed in organization name',
+            'contactno'=>'10 numeric characters allowed in phone No',
+            'city' =>'City name should be of max 20 characters',
+            'state'=>'State name should be of max 30 characters',
+            'address'=>'Max 255 characters allowed in address',
+            'quantity'=>'Numeric characters in hundereds allowed in quantity.'
+         
+        ]
+       );
+        
         //updates the value if the question exixts
         if(!empty($array)){
             DB::table('oxygen_donors')
